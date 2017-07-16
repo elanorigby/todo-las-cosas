8add3dd787c18548a59440101f4d57ad6a9d363c
Sharlak <elanorigby@gmail.com>
2017-07-16 15:22:59 +0100

    beginning to figure out click

--- todo/todo.py
+++ todo/todo.py
@@ -1,9 +1,9 @@
 import click
 
-@click.command()
+@click.group(invoke_without_command=True)
 @click.option('--done', '-d', help="move item to done list. item required.")
 @click.option('--remove', '-r', help="straight up delete item. item required." )
-@click.option('--resurrect', '-r', help="move item from done list to todo list")
+@click.option('--resurrect', help="move item from done list to todo list")
 @click.option('--amend', help="change item text. defaults to last item added.")
 def todo(done, remove, amend, resurrect):
     pass
