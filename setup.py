6420e4f3d798ae0c48f25dc2746d89dc8c9b8cba
Sharlak <elanorigby@gmail.com>
2017-07-16 15:15:43 +0100

    it's a start

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="todo-las-cosas",
+    version='0.1',
+    py_modules=['todo'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        todo=todo:todo
+    ''',
+)
+
+# copied from
+# https://kushaldas.in/posts/building-command-line-tools-in-python-with-click.html
