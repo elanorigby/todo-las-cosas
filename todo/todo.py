83c3d40ebdac6f16b0c9d85375fee0c372b744b0
Sharlak <elanorigby@gmail.com>
2017-07-31 14:41:34 +0100

    blank slate

--- todo/todo.py
+++ todo/todo.py
@@ -1,11 +1,2 @@
 import click
 
-@click.group(invoke_without_command=True)
-@click.option('--done', '-d', help="move item to done list. item required.")
-@click.option('--remove', '-r', help="straight up delete item. item required." )
-@click.option('--resurrect', help="move item from done list to todo list")
-@click.option('--amend', help="change item text. defaults to last item added.")
-def todo(done, remove, amend, resurrect):
-    pass
-
-
